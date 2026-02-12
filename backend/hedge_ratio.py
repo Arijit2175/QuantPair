69227f32924079ecf4eef7a29eb1e695e859ef3d
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-02-12 13:29:35 +0530

    function for hedge ratio estimation

--- backend/hedge_ratio.py
+++ backend/hedge_ratio.py
@@ -2,3 +2,11 @@ import numpy as np
 import pandas as pd
 from typing import Tuple
 from sklearn.linear_model import LinearRegression
+
+def estimate_hedge_ratio(price1: pd.Series, price2: pd.Series) -> float:
+    X = price2.values.reshape(-1, 1)
+    y = price1.values
+    model = LinearRegression().fit(X, y)
+    beta = model.coef_[0]
+    return beta
+
