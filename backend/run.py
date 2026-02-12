768909c491a7bd69a470572f481faf19b7e3ff3e
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-02-12 13:55:38 +0530

    installed required package

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,10 @@
 import pandas as pd
-from backend.pair_selection import select_pairs
-from backend.hedge_ratio import estimate_hedge_ratio
-from backend.spread_signal import compute_spread, compute_rolling_stats, compute_zscore
-from backend.trading_rules import generate_trade_signals
-from backend.risk_management import calculate_position_size, set_stop_loss_take_profit, assign_risk_level
-from backend.backtest import backtest_strategy
+from pair_selection import select_pairs
+from hedge_ratio import estimate_hedge_ratio
+from spread_signal import compute_spread, compute_rolling_stats, compute_zscore
+from trading_rules import generate_trade_signals
+from risk_management import calculate_position_size, set_stop_loss_take_profit, assign_risk_level
+from backtest import backtest_strategy
 
 def main():
     print("--- Pairs Trading Strategy Runner ---")
