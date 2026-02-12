4a83c9ebbeb5644cff18e546d8619f7982fd9522
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-02-12 13:23:53 +0530

    download the stock data

--- backend/data_acquisition.py
+++ backend/data_acquisition.py
@@ -2,3 +2,9 @@ import pandas as pd
 import yfinance as yf
 from typing import Tuple
 
+def download_stock_data(ticker: str, start: str, end: str) -> pd.DataFrame:
+    df = yf.download(ticker, start=start, end=end)[['Close']]
+    df = df.rename(columns={'Close': ticker})
+    df.index = pd.to_datetime(df.index)
+    return df
+
